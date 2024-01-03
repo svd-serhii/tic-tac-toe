592c46b910dacd992024f97f4a51d0204ae126ea
Serhii Demchenko <svdemchenko@gmail.com>
2024-01-03 20:14:22 +0200

    add Gameboard component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,21 +1,29 @@
 import React, { useState } from "react";
 
-const Player = ({ name, symbol }) => {
+const Player = ({ initialName, symbol }) => {
+	const [playerName, setPlayerName] = useState(initialName);
 	const [isEditing, setIsEditing] = useState(false);
 
 	function handleEditClick() {
 		setIsEditing((prevState) => !prevState);
 	}
 
-	let playerName = <span className="player-name">{name}</span>;
+	function handleChange(event) {
+		console.log(event);
+		setPlayerName(event.target.value);
+	}
+
+	let editablePlayerName = <span className="player-name">{playerName}</span>;
 	if (isEditing) {
-		playerName = <input type="text" required value={name} />;
+		editablePlayerName = (
+			<input type="text" required value={playerName} onChange={handleChange} />
+		);
 	}
 
 	return (
 		<li>
 			<span className="player">
-				{playerName}
+				{editablePlayerName}
 				<span className="player-symbol">{symbol}</span>
 			</span>
 			<button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
